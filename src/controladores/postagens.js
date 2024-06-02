60d2a938794a06ed704e8f73993986ed8c6f1779
jessica-dds <jessica.gleizer@hotmail.com>
2024-06-02 00:46:14 -0300

    Finalizado

--- src/controladores/postagens.js
+++ src/controladores/postagens.js
@@ -99,8 +99,65 @@ const comentar = async (req, res) => {
         return res.status(400).json(error.message);
     }
 }
+
+const feed = async (req, res) => {
+    const { id } = req.usuario;
+    const { offset } = req.query;
+
+    const o = offset ? offset : 0;
+
+    try {
+        // const postagens = knex('postagens').limit(10).offset(o);
+        const postagens = await knex('postagens')
+            .where('usuario_id', '!==', id)
+            .limit(10)
+            .offset(o);
+
+        if (postagens.length === 0) {
+            return res.status(200).json(postagens);
+        }
+
+        for (const postagem of postagens) {
+            // usuario 
+            const usuario = await knex('usuarios')
+                .where({ id: postagem.usuario_id })
+                .select('imagem', 'username', 'verificado')
+                .first();
+
+            postagem.usuario = usuario;
+
+            // fotos 
+            const fotos = await knex('postagem_fotos')
+                .where({ postagem_id: postagem.id })
+                .select('imagem');
+            postagem.fotos = fotos;
+
+            // curtidas
+            const curtidas = await knex('postagens_curtidas')
+                .where({ postagem_id: postagem.id })
+                .select('usuario_id');
+            postagem.curtidas = curtidas.length;
+
+            // curtido por mim
+            postagem.curtidoPorMim = curtidas.find(curtida => curtida.usuario_id === id) ? true : false;
+
+            // comentários
+            const comentarios = await knex('postagem_comentarios')
+                .leftJoin('usuarios', 'usuarios.id', 'postagem_comentarios.usuario_id')
+                .where({ postagem_id: postagem.id })
+                .select('usuarios.username', 'postagem_comentarios.texto');
+            postagem.comentarios = comentarios;
+        }
+
+        return res.status(200).json(postagens);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
 module.exports = {
     novaPostagem,
     curtir,
-    comentar
+    comentar,
+    feed
 }
